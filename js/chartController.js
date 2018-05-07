ccdeafcfac5930f80345df5266d34e09b2d64cff
akashom53 <akashom53@gmail.com>
2018-05-08 01:27:45 +0530

    Minor fixes

--- js/chartController.js
+++ js/chartController.js
@@ -1,11 +1,11 @@
 var ctx = document.getElementById('canvas').getContext('2d');
 
 var data = {
-  labels: ['Android (Java)', 'iOS (Objective C)', 'iOS (Swift)', 'Python', 'Django', 'JavaScript', 'Processing', 'Core Java', 'Unity'],
+  labels: ['Android (Java)', 'iOS (Objective C)', 'JavaScript', 'Python', 'Core Java', 'iOS (Swift)', 'Processing', 'Django', 'Unity'],
   datasets: [{
     backgroundColor: 'rgba(0, 255, 100, 0.5)',
     borderColor: 'rgba(0, 155, 0, 1)',
-    data: [90, 75, 55, 75, 40, 45, 60, 90, 65]
+    data: [90, 75, 45, 75, 90, 45, 60, 40, 65]
   }]
 }
 
