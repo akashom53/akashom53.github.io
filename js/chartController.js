a21a9127e29a95e0c63aeffe400afc9ccf3e1cdb
akashom53 <akashom53@gmail.com>
2018-05-07 19:59:19 +0530

    Skills done

--- js/chartController.js
+++ js/chartController.js
@@ -1,20 +1,32 @@
 var ctx = document.getElementById('canvas').getContext('2d');
 
 var data = {
-  labels: ['Running', 'Swimming', 'Eating', 'Cycling'],
+  labels: ['Android (Java)', 'iOS (Objective C)', 'iOS (Swift)', 'Python', 'Django', 'JavaScript', 'Processing', 'Core Java', 'Unity'],
   datasets: [{
-    data: [20, 10, 4, 2]
+    backgroundColor: 'rgba(0, 255, 100, 0.5)',
+    borderColor: 'rgba(0, 155, 0, 1)',
+    data: [90, 75, 55, 75, 40, 45, 60, 90, 65]
   }]
 }
+
 
 var myRadarChart = new Chart(ctx, {
   type: 'radar',
   data: data,
   options: {
     maintainAspectRatio: false,
+    legend: {
+      display: false
+    },
     scale: {
-      // Hides the scale
-      display: true
+      ticks: {
+        display: false,
+        suggestedMax: 100,
+        beginAtZero: true
+      },
+      pointLabels: {
+        fontSize: 15
+      }
     }
   }
 });
